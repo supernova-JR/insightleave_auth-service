fd87c2e6e5969ea789ff135fb9faecf71195bcb0
Lydia Zhang <leo.liu@vudoo.com>
2021-01-03 22:17:10 +1100

    create auth entity

---
+++ src/main/java/com/insightleave/authservice/entities/AuthEntity.java
@@ -0,0 +1,31 @@
+package com.insightleave.authservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "auth")
+public class AuthEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+}
